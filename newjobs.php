fbf65b8d9de0fd481efa7c975d82afa78f6e186c
jtimko <justin@jtimko.com>
2017-08-20 11:05:39 -0700

    Adding a form

--- newjobs.php
+++ newjobs.php
@@ -6,7 +6,12 @@
  ?>
     <div class="row">
         <div class="col-8 mx-auto">
-          <form action="#" 
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Email Address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+            </form>
         </div><!--end of col-->
     </div><!--end of row-->
 <?php require('footer.php');
